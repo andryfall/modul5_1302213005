048fd94c81f2ced8a6b61419a0c7c5fb6bafee75
fdsa <fdsaf@gmail.com>
2023-03-21 16:55:15 +0700

    “implementasi-generic-class

--- modul5_1302213005/modul5_1302213005/Program.cs
+++ modul5_1302213005/modul5_1302213005/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+internal class program
+{
+    static void Main(string[] args)
+    {
+        SimpleDataBase<double> data = new SimpleDataBase<double>();
+        data.AddNewData(13.0);
+        data.AddNewData(2.0);
+        data.AddNewData(5.0);
+        data.PrintAllData();
+    }
+}
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase() {
+        storedData= new List<T>();
+        inputDates= new List<DateTime>();
+    }
+
+    public void AddNewData(T input)
+    {
+        storedData.Add(input);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 1; i <= storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + i + " berisi: " + storedData[i-1] + ", yang disimpan pada waktu UTC: " + inputDates[i-1]);
+        }
+    }
+}
